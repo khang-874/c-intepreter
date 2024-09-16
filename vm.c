7237084b78a9d2ca154414998e686f0aeb129c51
Khang Tran <khang07087@gmail.com>
2024-09-16 18:52:49 -0400

    Add scanner and compiler

--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include "chunk.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 #include <stdio.h>
@@ -70,8 +71,7 @@ static InterpretResult run(){
 #undef READ_CONSTANT
 #undef BINARY_OP
 }
-InterpretResult interpret(Chunk *chunk){
-    vm.chunk = chunk;
-    vm.ip = vm.chunk -> code;
-    return run();
+InterpretResult interpret(const char* source){
+    compile(source);
+    return INTERPRET_OK;
 }
